f879bc74f543326064e13c98597031192f6d66bb
Benjamin Cooley <benjamincooley81@gmail.com>
2021-04-07 17:14:11 -0700

    add desktop background toggle

--- desktopdrawingp5.js
+++ desktopdrawingp5.js
@@ -1,23 +1,37 @@
 var pmousex, pmousey;
+var bg;
+var drawMode;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
   stroke(0);
   strokeWeight(5);
+  bg = loadImage("data/background.jpg");
+  drawMode = true;
 }
 
 function draw() {
-
-  if (mouseIsPressed) {
+  if (mouseIsPressed && drawMode) {
     line(mouseX, mouseY, pmousex, pmousey);
   }
-
-  if (keyIsPressed && key == ' ') {
-    background(255);
-  }
-  print(frameRate);
-
   pmousex = mouseX;
   pmousey = mouseY;
+}
+
+function keyPressed() {
+  if (key == ' ' && drawMode) {
+    background(255);
+  }
+  if (key == 'w') {
+    if (drawMode) {
+      image(bg, 0, 0, width, height);
+      drawMode = false;
+      noLoop();
+    } else {
+      background(255);
+      drawMode = true;
+      loop();
+    }
+  }
 }
